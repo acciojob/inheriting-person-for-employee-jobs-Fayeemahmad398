92ce07691158e1adbd232592b26741fe63593d92
Fayeemahmad398 <117560978+Fayeemahmad398@users.noreply.github.com>
2023-04-14 13:10:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-// complete this js code
+  to  Everyone
+Object.prototype.getKeys = function() {
+  return Object.keys
+// Person constructor function
 function Person(name, age) {
-	greet(){
-		console.log(`Hello, my name is ${name}, I am ${age} years old.`)
-	}
+  this.name = name;
+  this.age = age;
 }
 
+// Person prototype method
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+}
+
+// Employee constructor function
 function Employee(name, age, jobTitle) {
+  // call the Person constructor function and set its properties
+  Person.call(this, name, age);
 
+  // set the Employee-specific property
+  this.jobTitle = jobTitle;
+}
 
-	super();
-	jobGreet(){
-		console.log(`Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}.`)
-	}
-	
+// Inherit the Person prototype
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+// Employee prototype method
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
 
 // Do not change code below this line
